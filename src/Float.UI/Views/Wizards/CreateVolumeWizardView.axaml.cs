41f62b5d534fd3545181ceb8aa487bd4fa207c02
Abimael Miranda <mirandaabimael57@gmail.com>
2026-06-29 11:09:48 -0300

    UI polish: icon strokes, console style, volume actions
    Bind icon strokes to parent button foreground so they adapt to themes.
    Apply console style class to volume wizard output and auto-scroll on
    new output. Consolidate volume actions under an ACTIONS section header.

--- src/Float.UI/Views/Wizards/CreateVolumeWizardView.axaml.cs
+++ src/Float.UI/Views/Wizards/CreateVolumeWizardView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Float.UI.ViewModels.Wizards;
 
 namespace Float.UI.Views.Wizards;
 
@@ -7,5 +8,15 @@ public partial class CreateVolumeWizardView : UserControl
     public CreateVolumeWizardView()
     {
         InitializeComponent();
+
+        DataContextChanged += (_, _) =>
+        {
+            if (DataContext is CreateVolumeWizardViewModel vm)
+                vm.PropertyChanged += (_, e) =>
+                {
+                    if (e.PropertyName == nameof(CreateVolumeWizardViewModel.Output))
+                        OutputBox.CaretIndex = OutputBox.Text?.Length ?? 0;
+                };
+        };
     }
 }
