d9facf2e2f6d7916c07a09fb3ef9d02c11e00f45
Abimael Miranda <mirandaabimael57@gmail.com>
2026-06-29 18:38:50 -0300

    feat: add ContainerDetailViewModel with mounts and log streaming

---
+++ src/Float.UI/ViewModels/ContainerDetailViewModel.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Float.Core.Abstractions.Services;
+using Float.Core.Models;
+
+namespace Float.UI.ViewModels;
+
+public partial class ContainerDetailViewModel : ViewModelBase
+{
+    private readonly IContainerLogReader _logReader;
+    private readonly Action _goBack;
+    private CancellationTokenSource? _logsCts;
+
+    public ContainerItemViewModel Container { get; }
+    public IReadOnlyList<ContainerVolume> Volumes => Container.Source.Volumes;
+    public ObservableCollection<string> LogLines { get; } = [];
+
+    [ObservableProperty] public partial bool IsLoadingLogs { get; set; }
+
+    public ContainerDetailViewModel(ContainerItemViewModel container, IContainerLogReader logReader, Action goBack)
+    {
+        Container = container;
+        _logReader = logReader;
+        _goBack = goBack;
+    }
+
+    [RelayCommand]
+    private void Back() => _goBack();
+
+    public void StartLogs()
+    {
+        StopLogs();
+        LogLines.Clear();
+        _logsCts = new CancellationTokenSource();
+        var ct = _logsCts.Token;
+        IsLoadingLogs = true;
+        _ = Task.Run(async () =>
+        {
+            await _logReader.ReadLogsAsync(
+                Container.Name,
+                line => Dispatcher.UIThread.Post(() =>
+                {
+                    LogLines.Add(line);
+                    IsLoadingLogs = false;
+                }),
+                ct).ConfigureAwait(false);
+
+            await Dispatcher.UIThread.InvokeAsync(() => IsLoadingLogs = false);
+        }, ct);
+    }
+
+    public void StopLogs()
+    {
+        _logsCts?.Cancel();
+        _logsCts?.Dispose();
+        _logsCts = null;
+        IsLoadingLogs = false;
+    }
+}
